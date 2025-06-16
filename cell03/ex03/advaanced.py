e44afb65c2eff5d5d40436c3e85cc5cfacc877c2
ckongruk <chanachonchon.2019@gmail.com>
2025-06-16 08:47:00 +0000

    advaa

--- cell03/ex03/advaanced.py
+++ cell03/ex03/advaanced.py
@@ -4,7 +4,7 @@ def print_multiplication_tables():
 
     for i in range(1, 11):
         print(f"Table for {i}:")
-        for j in range(1 11):
+        for j in range(1, 11):
             result = i * j
             print(f" {i} x {i} = {result}")
         
