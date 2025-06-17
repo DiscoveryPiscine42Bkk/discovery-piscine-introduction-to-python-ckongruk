07d7ab81abf470d10e77eca37cbb0bfb2f89ed2a
ckongruk <chanachonchon.2019@gmail.com>
2025-06-17 04:12:26 +0000

    age

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -5,5 +5,4 @@ age_in_30_years = age + 30
 
 print(f"In 10 years, you will be {age_in_10_years} years old.")
 print(f"In 20 years, you will be {age_in_20_years} years old.")
-print(f"In 30 years, you will be {age_in_30_years} years old.")
-    
+print(f"In 30 years, you will be {age_in_30_years} years old.")    
